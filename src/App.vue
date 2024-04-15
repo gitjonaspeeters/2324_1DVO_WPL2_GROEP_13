a2b4771c9092c869cf953e7dad0a0fdb79495a99
Lukas Steelandt <lukas.steelandt@student.pxl.be>
2024-04-15 16:59:05 +0200

    project in vue app gezet

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<template>
+  <html lang="en">
+  <head>
+    <title>2324_1DVO_WPL2_GROEP_13</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
+  <!--Maak hier een mooi knop die naar de index van de teampagina gaat met css en bootstrap-->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Welkom op de teampagina van groep 13</h1>
+        <p>Op deze pagina kan je meer te weten komen over de leden van groep 13</p>
+        <a href="teampage/index.html" class="btn btn-primary">Naar de teampagina</a>
+      </div>
+    </div>
+  </div>
+  </body>
+  </html>
+</template>
+
+<style scoped>
+  
+</style>
