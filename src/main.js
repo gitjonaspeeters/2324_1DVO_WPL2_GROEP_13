0fbd8fb72073124af6b7bbe42f6e36a25fc0d8b2
EbrarYasliPXL <12201323@student.pxl.be>
2024-04-16 15:31:28 +0200

    navigation bar start finished

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 import App from './App.vue'
 import router from './router'
