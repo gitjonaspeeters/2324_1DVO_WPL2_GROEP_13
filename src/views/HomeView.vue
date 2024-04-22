4a301d06581f3e9ec32844c9abf3429885943985
EbrarYasliPXL <12201323@student.pxl.be>
2024-04-22 15:45:33 +0200

    banner afgewerkt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,15 @@
     <div v-for="(slide, index) in slides" :key="index" v-show="index === currentSlideIndex">
       <img :src="slide.imageUrl" style="width:100%">
       <div style="text-align:center">
+        <div class="head-content">
+          <h1>{{ slide.headTitle }}</h1>
+          <h2>{{ slide.headText }}</h2>
+          <button>{{ slide.headButton }}</button>
+        </div>
         <span class="dot" v-for="(slide, index) in slides" :key="index" @click="currentSlide(index)"
           :class="{ 'active': index === currentSlideIndex }"></span>
       </div>
-
     </div>
-
-
   </div>
 </template>
 
@@ -20,9 +22,9 @@ export default {
     return {
       currentSlideIndex: 0,
       slides: [
-        { imageUrl: "../../assets/Untitled-1.png", caption: "Caption Text" },
-        { imageUrl: "../../assets/Untitled-2.png", caption: "Caption Two" },
-        { imageUrl: "../../assets/Untitled-3.png", caption: "Caption Three" },
+        { imageUrl: "../../assets/Untitled-1.png", headTitle: "Nieuwe collectie", headText: "Probeer nu onze nieuwe winter collectie!", headButton: "Bekijk"},
+        { imageUrl: "../../assets/Untitled-2.png", headTitle: "Nieuwe collectie" , headText: "Probeer nu onze nieuwe winter collectie!", headButton: "Bekijk"},
+        { imageUrl: "../../assets/Untitled-3.png", headTitle: "Nieuwe collectie", headText: "Probeer nu onze nieuwe winter collectie!", headButton: "Bekijk" },
       ]
     };
   },
@@ -69,10 +71,34 @@ img {
   border-radius: 50%;
   display: inline-block;
   transition: background-color 0.6s ease;
+  position: relative;
+  bottom: 14rem;
 }
 
 .active {
   background-color: #717171;
 }
+/* content head */
+.head-content{
+    position: relative;
+    bottom: 17rem;
+    left: 7rem;
+    text-align: left;
+    font-family: Georgia, sans-serif;
+    max-width: 22rem;
+}
+.head-content h1{
+    font-size: 3rem;
+}
+.head-content h2{
+  font-size: 1.8rem;
 
+  margin-bottom: 0.5rem;
+  margin-top: -0.5rem;
+}
+.head-content button{
+  padding: 0.3rem 1.3rem;
+  font-size: 1.8rem;
+  border-radius: 1.8rem;
+}
 </style>
