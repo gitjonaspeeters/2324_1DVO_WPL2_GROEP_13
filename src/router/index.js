5d720edfdaa4b1c14c3698a46153a4f22899f773
Matthias Billen <matthiasbillenn@gmail.com>
2024-04-23 16:41:28 +0200

    Categorie toegevoegd aand de navigatie en index

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
-import MainView  from "../views/Main.vue";
-import TeampageView from "../views/TeampageView.vue"
-
+import MainView from "../views/Main.vue";
+import TeampageView from "../views/TeampageView.vue";
+import CategorieView from "../views/CategorieView.vue";
+import RuimtesView from "../views/RuimtesView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/categorie",
+      name: "categorie",
+      component: CategorieView,
+    },
+    {
+      path: "/ruimtes",
+      name: "ruimtes",
+      component: RuimtesView,
+    },
     {
       path: "/",
       name: "main",
@@ -25,8 +36,8 @@ const router = createRouter({
     {
       path: "/teampage",
       name: "teampage",
-      component: TeampageView
-    }
+      component: TeampageView,
+    },
   ],
 });
 
