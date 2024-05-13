54ecf0997a3982a21a1f64a0e830e62b38951678
Lukas Steelandt <lukas.steelandt@student.pxl.be>
2024-05-13 13:42:57 +0200

    router gefxt

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 
@@ -8,7 +8,6 @@ import RuimtesView from "../views/RuimtesView.vue";
 import CartView from "../views/WinkelwagenView.vue";
 
 
-
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import AccountView from "../views/AccountView.vue";
@@ -16,66 +15,71 @@ import DetailView from "@/views/DetailView.vue";
 
 
 import WichlistView from "@/views/WishlistView.vue";
-
+import formulierView from "@/views/FormulierView.vue"
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/categorie",
-      name: "categorie",
-      component: CategorieView,
-    },
-    {
-      path: "/ruimtes",
-      name: "ruimtes",
-      component: RuimtesView,
-    },
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: AboutView,
-    },
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/categorie",
+            name: "categorie",
+            component: CategorieView,
+        },
+        {
+            path: "/ruimtes",
+            name: "ruimtes",
+            component: RuimtesView,
+        },
+        {
+            path: "/",
+            name: "home",
+            component: HomeView,
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: AboutView,
+        },
 
-    {
-      path: "/login",
-      name: "login",
-      component: LoginView,
-    },
-    {
-      path: "/register",
-      name: "register",
-      component: RegisterView,
-    },
-    {
-      path: "/account",
-      name: "account",
-      component: AccountView,
-    },
-    {
+        {
+            path: "/login",
+            name: "login",
+            component: LoginView,
+        },
+        {
+            path: "/register",
+            name: "register",
+            component: RegisterView,
+        },
+        {
+            path: "/account",
+            name: "account",
+            component: AccountView,
+        },
+        {
 
-      path: "/cart",
-      name: "cart",
-      component: CartView,
-    },
-    {
-      path: "/product",
-      name: "product",
-      component: DetailView,
+            path: "/cart",
+            name: "cart",
+            component: CartView,
+        },
+        {
+            path: "/product",
+            name: "product",
+            component: DetailView,
 
-    },{
-    path: "/wishlist",
-    name: "wishlist",
-    component: WichlistView,
-  },
-
-  ],
+        },
+        {
+            path: "/wishlist",
+            name: "wishlist",
+            component: WichlistView,
+        },
+        {
+            path: "/form",
+            name: "form",
+            component: formulierView,
+        }
+    ],
 });
 
 export default router;
