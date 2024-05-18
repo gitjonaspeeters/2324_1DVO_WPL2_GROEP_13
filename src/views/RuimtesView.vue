ea5841dddde986ed8c5c6c8d4963726be6d2d7e1
Matthias Billen <matthiasbillenn@gmail.com>
2024-05-18 16:10:54 +0200

    test 3 voor alle images op netlify te krijgen

--- src/views/RuimtesView.vue
+++ src/views/RuimtesView.vue
@@ -176,7 +176,7 @@
   .bedroom{
     background-size: cover;
     background-repeat: no-repeat;
-    background-image: url("./assets/vanaf-1699-euro.jpeg");
+    background-image: url("@/assets/vanaf-1699-euro.jpeg");
     
     width: 777px;
     height: 673px;
